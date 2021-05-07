566dcf1076d6e6ab393481bf4b99241a6b749fa5
Laba Kumar Deka <labakumardeka42@gmail.com>
2021-05-07 18:01:53 +0530

    added notification alert for assam kamrup
    hard coded a simple notification system for alerts on vaccination slots

--- getSlots.py
+++ getSlots.py
@@ -1,21 +1,44 @@
 import requests
-
+from playsound import playsound
+import time
 
 def main():
-    getStates()
-   
+   while True:
+       alert()
+       time.sleep(120)
 
 
 
+def alert():
+    #states = getStates() used this to get state id for assam
+    #districts = getDistricts()used this to get district id for kamrup metro
+    slots = getSlots()
+    #playsound('alert.mp3')
+    for x in slots:
+        sessions = slots[x] 
+        print(sessions)
+        no = len(sessions)
+        print(len(sessions))
+        if no>0:
+            playsound('salert.mp3')
 
 
 def getStates():
     response = requests.get('https://cdn-api.co-vin.in/api/v2/admin/location/states')
     json_data = response.json()
-    print(json_data)
-    
+    return json_data
 
-getStates()
+
+def getDistricts():
+    response = requests.get('https://cdn-api.co-vin.in/api/v2/admin/location/districts/4')
+    json_data = response.json()
+    return json_data
+
+def getSlots():
+    response = requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=49&date=03-05-2021 ')
+    json_data = response.json()
+    return json_data
+
 
 if __name__ == "__main__":
     main()
